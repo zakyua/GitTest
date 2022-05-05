217df15705834093d25eacb4bff38a61d4be80d2
Chen <chen@163.com>
2022-05-05 20:05:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/Git/gitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.Git;
+
+/**
+ * @author ccstart
+ * @create 2022-05-05 20:04
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
