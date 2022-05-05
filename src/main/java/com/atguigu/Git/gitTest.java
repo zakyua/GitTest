e5cad08caa998088a409ad2e1df56f21443e54c1
Chen <chen@163.com>
2022-05-05 20:27:31 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/Git/gitTest.java
+++ src/main/java/com/atguigu/Git/gitTest.java
@@ -6,9 +6,15 @@ package com.atguigu.Git;
  */
 public class gitTest {
     public static void main(String[] args) {
+
         System.out.println("hello,git");
         System.out.println("hot_fix");
 
         System.out.println("我是hot-fix 分支");
+        System.out.println("我是hot-fix 分支2");
+
+        System.out.println("xixi ");
+
+        System.out.println("我是hot-fix分支 ，这是我的提交");
     }
 }
